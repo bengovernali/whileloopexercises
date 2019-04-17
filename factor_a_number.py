dfe4f7c2d8c8875ddfd8fc629f1f67b67463af13
Ben Governali <bgovern93@gmail.com>
2019-04-17 15:32:49 -0400

    completed factor_a_number.py

---
+++ factor_a_number.py
@@ -0,0 +1,10 @@
+number = int(input("Please enter a number: "))
+i = number
+
+print("Factors of %d: " % number)
+
+while i > 0:
+    if number % i == 0:
+        factor1 = number / i
+        print("%d and %d" % (i, factor1))
+    i -= 1    
